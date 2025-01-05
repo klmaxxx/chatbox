1f63ca70ad356b1b03a7615d82e3830f524dc3fd
klmaxxx <maxgoose96@gmail.com>
2025-01-05 22:18:39 +0200

    some more little changes

--- chatbox_server.py
+++ chatbox_server.py
@@ -1,11 +1,25 @@
-from flask import Flask, session, redirect, url_for
+from flask import Flask, sessions, url_for, redirect 
 
 chatbox_main = "chatbox/chatbox_main.html"
 
+
 def main_page():
-    return chatbox_main
+    return '''<!DOCTYPE html>
+    <html>
+        <head>
+            <title>chatbox</title>
+        </head></html
+        <body>
+        <h1> chatbox </h1>
+        </body>
+    </html>
+    '''
+
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "14885269yateshakalitkurazebu"
+
+app.add_url_rule("/chatbox", "main_page", main_page)
 
 if __name__ == "__main__":
     app.run()
