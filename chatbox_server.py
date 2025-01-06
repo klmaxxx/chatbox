f6a3058a75625a3cb090f8d083a8a9577ecdde88
klmaxxx <maxgoose96@gmail.com>
2025-01-06 20:22:17 +0200

    FUCKING DATABASE

--- chatbox_server.py
+++ chatbox_server.py
@@ -1,5 +1,9 @@
-from flask import Flask, sessions, url_for, redirect, request
 import os
+
+from flask import Flask, redirect, request, url_for
+
+from chatbox_userdata import fill_info
+
 import sqlite3
 
 # chatbox_main = "chatbox/chatbox_main.html"
@@ -9,19 +13,20 @@ folder = os.getcwd()
 app = Flask(__name__, static_folder=folder)
 app.config["SECRET_KEY"] = "14885269yateshakalitkurazebu"
 
-def main_page():
-    return redirect(url_for('static', filename="chatbox_main.html"))
-
 def user_info():
-    username = request.form['username1']
-    email = request.form['email1']
-    password = request.form['password1']
+    username = request.form["username1"]
+    email = request.form["email1"]
+    password = request.form["password1"]
+    fill_info()
 
+def main_page():
+    return redirect(url_for("static", filename="chatbox_main.html"))
 
-@app.route('/')
-@app.route('/save_data', methods=['POST'])
+app.route("/")
+app.route("/save_data", methods=["POST"])
 
-app.add_url_rule("/", "main_page", main_page)
+app.add_url_rule("/main_page", "main_page", main_page)
 
 if __name__ == "__main__":
     app.run()
+    user_info()
