ed47ae411c48101a9692ce2a2bf06ac1415c5431
klmaxxx <maxgoose96@gmail.com>
2025-01-08 16:03:54 +0200

    some more changes and still not working correctly

--- chatbox_scripts.py
+++ chatbox_scripts.py
@@ -3,10 +3,12 @@ import codecs
 a = codecs.open("chatbox_signup.html", "r", "utf-8")
 signupmenu = a.read()
 
+b = codecs.open("chatbox_main.html", "r", "utf-8")
+main_page_html = b.read
+
 print(signupmenu)
 
 
 def show_password1():
-    return signupmenu
     if '<input type="radio" name="showpassword1 value="Show password>"':
-        return '<input type="text" name = "password1 placeholder="Password">'
+            return '<input type="text" name = "password1 placeholder="Password">'
--- chatbox_server.py
+++ chatbox_server.py
@@ -2,7 +2,7 @@ import os
 
 from flask import Flask, redirect, url_for
 
-from chatbox_scripts import show_password1
+from chatbox_scripts import show_password1, main_page_html
 
 import codecs
 
@@ -28,18 +28,13 @@ app.config["SECRET_KEY"] = "14885269yateshakalitkurazebu"
 def main_page():
     return redirect(url_for("static", filename="chatbox_main.html"))
 
-
-def sign_up_page():
-    return redirect(url_for("static", filename="chatbox_signup.html"))
-
-
-
 # app.route("/")
 # app.route("/save_data", methods=["POST"])
 
 app.add_url_rule("/", "main_page", main_page)
-app.add_url_rule("/sign_up_page", "sign_up_page", sign_up_page)
 
 if __name__ == "__main__":
     app.run()
     # user_info()
+    if main_page():
+        
