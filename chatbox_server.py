f75c92613bce0d5d2dce02c9557f24496844f316
klmaxxx <maxgoose96@gmail.com>
2025-02-01 13:34:04 +0200

    started making the blog page

--- chatbox_server.py
+++ chatbox_server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 from chatbox_userdata import fill_info
 
@@ -15,8 +15,9 @@ def user_info():
     if request.method == "POST":
         user_info = {"username": request.form.get("username1"), "password": request.form.get("password1"), "email": request.form.get("email1")}
         fill_info(user_info["username"], user_info["email"], user_info["password"])
-        return main_page()
-    
+        return redirect(url_for("mainblog"))
+
+
 def main_page():
     return render_template("chatbox_main.html")
 
@@ -28,13 +29,16 @@ def signup():
 def signin():
     return render_template("chatbox_signin.html")
 
+def main_blog():
+    return render_template("chatbox_mainblog.html")
 
 # app.route("/")
 # app.route("/save_data", methods=["POST"])
 
 app.add_url_rule("/", "main_page", main_page)
-app.add_url_rule("/signup", "signup", signup)
-app.add_url_rule("/signin", "signin", signin)
+app.add_url_rule("/signup", "signup", signup, methods=["post", "get"])
+app.add_url_rule("/signin", "signin", signin, methods=["post", "get"])
+app.add_url_rule("/main_blog", "mainblog", main_blog, methods=["post", "get"])
 
 
 if __name__ == "__main__":
