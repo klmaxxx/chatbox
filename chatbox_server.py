0ba56ad35a10e0ca50b2dd55dab3f8a33942075f
klmaxxx <maxgoose96@gmail.com>
2025-02-12 13:37:18 +0200

    added friends pages(fake), AND I FINALLY FINISHED STYLES(almost, I will have to do them for the blog page for posts)

--- chatbox_server.py
+++ chatbox_server.py
@@ -1,6 +1,10 @@
+import logging
+
 from flask import Flask, redirect, render_template, request, url_for
 
-from chatbox_userdata import fill_info
+from chatbox_userdata import fill_info, check_info
+
+logging.basicConfig(level=logging.DEBUG)
 
 # import sqlite3
 
@@ -26,7 +30,7 @@ app.config["SECRET_KEY"] = "14885269yateshakalitkurazebu"
 # def log_in():
 #     if request.method == "POST":
 #         return redirect(url_for("mainblog"))
-   
+
 
 def main_page():
     return render_template("chatbox_main.html")
@@ -34,15 +38,20 @@ def main_page():
 
 def signup():
     if request.method == "POST":
-        fill_info(request.form.get("username1"), request.form.get("email1"), request.form.get("password1"))
-        return redirect(url_for("mainblog"))
+        if request.form.get("confirm_password1") != request.form.get("password1"):
+            return render_template("chatbox_signup.html")
+        else:
+            fill_info(
+            request.form.get("username1"), request.form.get("email1"), request.form.get("password1")
+                )
+            return redirect(url_for("mainblog"))
     else:
         return render_template("chatbox_signup.html")
 
 
 def signin():
     if request.method == "POST":
-        return redirect(url_for("mainblog"))
+        check_info(request.form.get("email2"), request.form.get("password2"), redirect(url_for("mainblog")), render_template("chatbox_signin.html"))
     else:
         return render_template("chatbox_signin.html")
 
@@ -51,7 +60,11 @@ def main_blog():
     if request.method == "POST":
         user_post_text = request.form.get("blog1")
         user_post_file = request.form.get("file1")
+        return render_template("chatbox_mainblog.html")
     return render_template("chatbox_mainblog.html")
+
+def friends_page():
+    return render_template('friend-page.html')
 
 # app.route("/")
 # app.route("/save_data", methods=["POST"])
@@ -60,7 +73,7 @@ app.add_url_rule("/", "main_page", main_page)
 app.add_url_rule("/signup", "signup", signup, methods=["post", "get"])
 app.add_url_rule("/signin", "signin", signin, methods=["post", "get"])
 app.add_url_rule("/main_blog", "mainblog", main_blog, methods=["post", "get"])
-
+app.add_url_rule("/friends_page", "friends_page", friends_page)
 
 if __name__ == "__main__":
     app.run(host=("0.0.0.0"), debug=True)
--- chatbox_userdata.py
+++ chatbox_userdata.py
@@ -1,12 +1,40 @@
-import sqlite3 
+import sqlite3
+from contextlib import closing
+from typing import Any
 
-db_name = "quiz.sqlite"
-conn = sqlite3.connect(db_name)
-cursor = conn.cursor()
+DB_NAME = "user_data.sqlite"
 
-#user info table(I hope it'll work)
-cursor.execute("CREATE TABLE IF NOT EXISTS user_info(username PRIMARY KEY, email VARCHAR, password VARCHAR)")
 
-def fill_info(username, email, password):
-    cursor.execute("INSERT INTO user_info(username, email, password) VALUES (username1, email1, password1)")
-    conn.commit()
+def execute(query: str, params: list[Any] | None = None):
+    """Виконує один запит з необов'язковими параметрами."""
+
+    with closing(sqlite3.connect(DB_NAME)) as conn:
+        with conn:  # Автоматичне збереження (commit) змін
+            with closing(conn.cursor()) as cursor:
+                cursor.execute(query, params or [])
+                return cursor.fetchall()  # Повертає всі результати для SELECT-запитів.
+
+
+# user info table(I hope it'll work)
+execute(
+    "CREATE TABLE IF NOT EXISTS user_info(id INTEGER PRIMARY KEY, username VARCHAR, email VARCHAR, password VARCHAR)"
+)
+
+
+def fill_info(username: str, email: str, password: str):
+    execute(
+        "INSERT INTO user_info(username, email, password) VALUES (?, ?, ?)",
+        [username, email, password],
+    )
+    print(*execute("SELECT * FROM user_info"), sep="\n", flush=True)
+
+def check_info(username, password, redirect, error):
+    cursor = conn.cursor()
+    execute("SELECT username FROM user_info")
+    result1 = cursor.fetchall() 
+    execute("SELECT password FROM user_info")
+    result2 = cursor.fetchall()
+    if username in result1 and password in result2:
+        return redirect
+    else:
+        return error
