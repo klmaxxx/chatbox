a4455478894a4894c33b6cb0590e393274f04aa0
klmaxxx <maxgoose96@gmail.com>
2025-02-15 11:07:47 +0200

    everything is done now.

--- chatbox_server.py
+++ chatbox_server.py
@@ -1,7 +1,6 @@
 from flask import Flask, redirect, render_template, request, session, url_for
 
-from chatbox_blogs import fill_blogs
-from chatbox_userdata import fill_info, get_user, user_exists, valid_user
+from chatbox_userdata import fill_info, fill_posts, get_posts, get_user, user_exists, valid_user
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "14885269yateshakalitkurazebu"
@@ -30,6 +29,7 @@ def signup():
             u_id, name, *_ = get_user(request.form.get("username"))
             session["user_id"] = u_id
             session["username"] = name
+            session["page"] = 1
             return redirect(url_for("mainblog"))
         return render_template("chatbox_signup.html", error=error)
 
@@ -47,16 +47,36 @@ def signin():
             u_id, name, *_ = get_user(request.form.get("username"))
             session["user_id"] = u_id
             session["username"] = name
+            session["page"] = 1
             return redirect(url_for("mainblog"))
 
 
 def main_blog():
-    if request.method == "POST":
-        user_post_file = request.form.get("file1")
-        fill_blogs(request.form.get("blog1"))
-        return render_template("chatbox_mainblog.html")
-    return render_template("chatbox_mainblog.html")
-    
+    if request.method == "POST" and "username" in session:
+        error = ""
+        posts = []
+        if len((text := request.form.get("text")).strip()) <= 5:
+            error = "Post text length must be greater than 5 symbols and not empty!"
+        else:
+            fill_posts(session["user_id"], text)
+            posts = get_posts(10, session["page"])
+        return render_template("chatbox_mainblog.html", error=error, posts=posts, session=session)
+    else:
+        posts = get_posts(10, session["page"])
+        return render_template("chatbox_mainblog.html", error="", posts=posts, session=session)
+
+
+def next_page():
+    if "username" in session:
+        session["page"] += 1
+    return redirect(url_for("mainblog"))
+
+
+def prev_page():
+    if "username" in session:
+        session["page"] -= 1
+    return redirect(url_for("mainblog"))
+
 
 def friends_page():
     return render_template("friend-page.html")
@@ -73,6 +93,8 @@ app.add_url_rule("/", "main_page", main_page)
 app.add_url_rule("/signup", "signup", signup, methods=["post", "get"])
 app.add_url_rule("/signin", "signin", signin, methods=["post", "get"])
 app.add_url_rule("/main_blog", "mainblog", main_blog, methods=["post", "get"])
+app.add_url_rule("/next_page", "next_page", next_page)
+app.add_url_rule("/prev_page", "prev_page", prev_page)
 app.add_url_rule("/friends_page", "friends_page", friends_page)
 app.add_url_rule("/logout", "logout", logout)
 
--- chatbox_userdata.py
+++ chatbox_userdata.py
@@ -15,24 +15,27 @@ def execute(query: str, params: list[Any] | None = None):
                 return cursor.fetchall()  # Повертає всі результати для SELECT-запитів.
 
 
-# user info table(I hope it'll work)
-execute(
-    "CREATE TABLE IF NOT EXISTS user_info(id INTEGER PRIMARY KEY, username VARCHAR, email VARCHAR, password VARCHAR)"
-)
+def create_tables():
+    execute(
+        "CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, username VARCHAR NOT NULL, email VARCHAR NOT NULL, password VARCHAR NOT NULL)"
+    )
+    execute(
+        "CREATE TABLE IF NOT EXISTS posts(id INTEGER PRIMARY KEY, author_id INTEGER NOT NULL, created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, text VARCHAR NOT NULL, FOREIGN KEY (author_id) REFERENCES users (id))"
+    )
 
 
 def fill_info(username: str, email: str, password: str):
     execute(
-        "INSERT INTO user_info(username, email, password) VALUES (?, ?, ?)",
+        "INSERT INTO users(username, email, password) VALUES (?, ?, ?)",
         [username, email, password],
     )
-    print(*execute("SELECT * FROM user_info"), sep="\n", flush=True)
+    print(*execute("SELECT * FROM users"), sep="\n", flush=True)
 
 
 def valid_user(username, password):
     return bool(
         execute(
-            "SELECT ROWID, * FROM user_info WHERE username == ? AND password == ? LIMIT 1",
+            "SELECT ROWID, * FROM users WHERE username == ? AND password == ? LIMIT 1",
             [username, password],
         )
     )
@@ -40,13 +43,35 @@ def valid_user(username, password):
 
 def user_exists(name, email):
     return bool(
-        execute("SELECT * FROM user_info WHERE username == ? OR email == ? LIMIT 1", [name, email])
+        execute("SELECT * FROM users WHERE username == ? OR email == ? LIMIT 1", [name, email])
     )
 
 
 def get_all_users():
-    return execute("SELECT ROWID, * FROM user_info")
+    return execute("SELECT ROWID, * FROM users")
 
 
 def get_user(name):
-    return execute("SELECT ROWID, * FROM user_info WHERE username == ? LIMIT 1", [name])[0]
+    return execute("SELECT * FROM users WHERE username == ? LIMIT 1", [name])[0]
+
+
+def fill_posts(user_id, text):
+    execute("INSERT INTO posts (author_id, text) VALUES (?, ?)", [user_id, text])
+
+
+def get_posts(limit=20, offset=0):
+    return execute(
+        """
+        SELECT username, text 
+        FROM posts 
+        JOIN users ON users.id = posts.author_id 
+        ORDER BY posts.created DESC 
+        LIMIT ? OFFSET ?
+        """,
+        [limit, (offset - 1) * limit],
+    )
+
+
+if __name__ == "__main__":
+    # create_tables()
+    pass
